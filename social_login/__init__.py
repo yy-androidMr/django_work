46361d445ae3fdfcfd48b5391ea52e28cc2e3e18
yy-androidMr <1226341090@qq.com>
2018-04-21 01:22:42 +0800

    删除无用
    delete

--- social_login/__init__.py
+++
@@ -1,4 +0,0 @@
-# -*- coding: utf-8 -*-
-
-version_info = (0, 2, 0)
-VERSION = __version__ = '.'.join( map(str, version_info) )
--- uwxLoginTest/wxlogin/apps.py
+++
@@ -1,5 +0,0 @@
-from django.apps import AppConfig
-
-
-class WxloginConfig(AppConfig):
-    name = 'wxlogin'
