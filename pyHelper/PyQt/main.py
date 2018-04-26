aef74a1b8f5759a0ca9cb87072f2845c6887a2da
yy-androidMr <1226341090@qq.com>
2018-04-26 18:22:00 +0800

    1
    1

--- pyHelper/PyQt/main.py
+++ pyHelper/PyQt/main.py
@@ -4,13 +4,8 @@ from PyQt5.QtWidgets import QApplication
 from PyQt.windows.main.MainControl import MainControl
 from PyQt.windows.main.tab.Tab1 import Tab1
 
-if __name__=='__main__':
 if __name__ == '__main__':
     app = QApplication(sys.argv)
     main_window = MainControl()
     main_window.show()
-<<<<<<< HEAD
-    sys.exit(app.exec_())    sys.exit(app.exec_())
-=======
->>>>>>> 1a5cfb28d17c7bb9d7f89d41420343b43557f571
     sys.exit(app.exec_())
