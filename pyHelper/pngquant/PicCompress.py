786af444d47f3e18c0085bed5e673a53957e583d
yy-androidMr <1226341090@qq.com>
2018-05-04 02:54:39 +0800

    1
    1

--- pyHelper/pngquant/PicCompress.py
+++ pyHelper/pngquant/PicCompress.py
@@ -34,23 +34,5 @@ def src2middle():
     cmd = 'for i in '+src+'/*.jpg;do jpegoptim -m50 -d ' + desc + ' -p "$i";done'
     os.system(cmd)
 
-    for root, dirs, files in os.walk(src):
-        for dir in dirs:
-            src_path = os.path.join(root, dir).replace('\\', '/')
-
-        for file in files:
-            src_file = os.path.join(root, file).replace('\\', '/')
-            extension = os.path.splitext(src_file)[1]
-            md5 = get_md5(src_file)
-            # print('%s,md5:%s,exten:%s' % (src_file, md5, extension))
-            if is_mac():
-                pass
-                # cmd = './pngquant-linux/pngquant --f --o %s/%s%s --quality 60-80 --speed=1 %s' % (
-                #     desc, md5, extension, src_file)
-                # print(cmd)
-                # os.system(cmd)
-
-
-
 if __name__ == '__main__':
     src2middle()
