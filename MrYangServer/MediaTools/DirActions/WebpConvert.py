5eedc95b9463e7e2293af2a1a4778b9e84948485
yy-androidMr <1226341090@qq.com>
2018-06-24 17:06:05 +0800

    1
    1

---
+++ MrYangServer/MediaTools/DirActions/WebpConvert.py
@@ -0,0 +1,17 @@
+import os
+
+D_WEBP_PATH = r'F:\django_work\pyHelper\Plugins\webp\bin\dwebp.exe'
+SRC = r'E:\cache\缓冲\2018_6_23'
+
+
+def webp2pic():
+    for root, dirs, files in os.walk(SRC):
+        for file in files:
+            if '.webp' in file:
+                source_path = os.path.join(root, file).replace('\\', '/')
+                os.system(D_WEBP_PATH + ' ' + source_path + ' -o ' + source_path + '.png')
+                os.remove(source_path)
+
+
+if __name__ == '__main__':
+    webp2pic()
