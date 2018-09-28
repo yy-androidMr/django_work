cdf98a0c195f383b55786e4c3925e24452fd39c5
yy-androidMr <1226341090@qq.com>
2018-09-28 19:17:53 +0800

    1
    1

--- MrYangServer/frames/xml/XMLGallery.py
+++ MrYangServer/frames/xml/XMLGallery.py
@@ -1,10 +1,16 @@
 # -*-coding:utf-8 -*-
 
-#每个解析都有个名字.对应configs_info配置中的configs_info->list
-from . import XMLBase
+# 每个解析都有个名字.对应configs_info配置中的configs_info->list
+from frames.xml import XMLBase
 
 CONFIG_NAME = 'gallery_info'
 
-def append():
+
+def append_ifnot_exist():
     path = XMLBase.c_path(CONFIG_NAME)
-    print(path)
+
+    info_root = XMLBase.parseXML(path)
+    for elem in info_root.findall('gallery'):
+        pass
+    # gallerys =
+    print(gallerys)
