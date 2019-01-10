27716024ac4d338ad86e6c8f0933eacc04f49b17
yy-androidMr <1226341090@qq.com>
2019-01-10 19:29:32 +0800

    1
    1

--- MrYangServer/MrYangServer/settings.py
+++ MrYangServer/MrYangServer/settings.py
@@ -11,7 +11,9 @@ https://docs.djangoproject.com/en/1.10/ref/settings/
 """
 
 import os
+from frames import Globals
 
+Globals.run_init()
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
@@ -127,6 +129,7 @@ DATABASES = {
     #     'PORT': '3306',
     # },
 }
+
 DATABASE_ROUTERS = ['MrYangServer.database_router.DatabaseAppsRouter']
 DATABASE_APPS_MAPPING = {
     # example:
