331d2447c88b10a8271bebd78eaae58d81f84239
WSH9UGKOTZZ0BFW\Administrator <1226341090@qq.com>
2019-06-16 16:11:09 +0800

    1

--- MrYangServer/MryangService/ServiceHelper.py
+++ MrYangServer/MryangService/ServiceHelper.py
@@ -5,6 +5,12 @@ def compair(left_path, right_path):
     left_list = ypath.releative_list(left_path)
     right_list = ypath.releative_list(right_path)
     # db_desc = [desc.desc_path for desc in db_list]
+    with open('left.txt', 'w+') as f:
+        for item in left_list:
+            f.write(item + '\n')
+    with open('right.txt', 'w+') as f:
+        for item in right_list:
+            f.write(item + '\n')
     delete_files = list(set(left_list).difference(set(right_list)))
     # for delete in delete_files:
     #     print(delete)
