8ee8c7c99505b283a04b7055bf694754871d6412
WSH9UGKOTZZ0BFW\Administrator <1226341090@qq.com>
2019-06-16 20:14:19 +0800

    1

--- MrYangServer/MryangService/ServiceHelper.py
+++ MrYangServer/MryangService/ServiceHelper.py
@@ -7,9 +7,17 @@ def compair_db(dbs, desc_path):
     file_list = ypath.list_folder(desc_path, include_dir=False)
     db_desc_list = [db.desc_path for db in dbs]
     diff = set(file_list).difference(set(db_desc_list))
+    with open('left.txt', 'w+', encoding='utf-8') as f:
+        for item in file_list:
+            f.write(item + '\n')
+    with open('right.txt', 'w+', encoding='utf-8') as f:
+        for item in db_desc_list:
+            f.write(item + '\n')
+
     with open('diff.txt', 'w+', encoding='utf-8') as f:
         for item in diff:
             f.write(item + '\n')
+    return diff
     # for desc_file in file_list:
     #     if desc_file not in db_desc_list:
     #         print('需要删除的:' + desc_file)
