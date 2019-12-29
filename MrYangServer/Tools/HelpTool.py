46e5c662e69a50909d2db97ae4188a2bbe30bbc0
WSH9UGKOTZZ0BFW\Administrator <1226341090@qq.com>
2019-12-29 22:08:26 +0800

    去重工作.

---
+++ MrYangServer/Tools/HelpTool.py
@@ -0,0 +1,44 @@
+import os
+
+from frames import yutils
+import stat
+
+
+def delrepeat_file_list(dir_list, max_size: int = 0):
+    repeat_file = {}
+    md5_list = {}
+    for dir in dir_list:
+        print('查重该目录:' + dir)
+        for root, dirs, files in os.walk(dir):
+            for file in files:
+                source_rela_path = os.path.join(root, file)
+                if max_size != 0:
+                    if os.stat(source_rela_path).st_size > max_size:
+                        continue
+                file_md5 = yutils.get_md5(source_rela_path)
+                if file_md5 in md5_list:
+                    repeat_file[source_rela_path] = md5_list[file_md5]
+                    print('找到重复:' + md5_list[file_md5])
+                else:
+                    md5_list[file_md5] = os.path.abspath(source_rela_path)
+        cmd = input('是否确认删除该目录下的重复文件?\n')
+        if(cmd == 'y'):
+            for file in repeat_file:
+                if os.path.exists(file):
+                    os.chmod(file, stat.S_IWRITE)
+                    os.remove(file)
+        else:
+            print('不处理该路径:'+dir)
+    print('delrepeat_file done')
+
+
+paths = []
+while True:
+    path = input('输入路径{不输入路径则继续}:')
+    if path is None or len(path) == 0:
+        print('开始去重中...')
+        break
+    if (os.path.exists(path)):
+        print('添加路径成功')
+        paths.append(path)
+    delrepeat_file_list(paths)
--- MrYangServer/test/test4.py
+++ MrYangServer/test/test4.py
@@ -2,8 +2,13 @@
 # out =subprocess.check_output('ping www.baidu.com',shell=True)
 # print(out.decode('gbk'))
 # print(decode('utf-8'))
-dd = {1: 2, 2: 3, 3: 4}
-ks = list(dd.keys())
-for key in ks:
-    del dd[key]
-print(dd)
+import os
+import time
+
+from frames import yutils, ypath
+
+# os.utime(r'F:\cache\AutoCAD2017 for MAC\1568603151319.mp4',(1572602407,1572602407))
+
+
+
+ypath.delrepeat_file(r"Z:\缓存路径\unzip")
